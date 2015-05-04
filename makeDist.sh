01ea3bf8b9e94ce155889ae2bbb167b2fa14ee56
Peter Cottle <pcottle@fb.com>
2015-05-04 13:26:09 -0700

    bash update

--- makeDist.sh
+++ makeDist.sh
@@ -7,13 +7,4 @@ sed -i '' -e "s#url .*#url \"https://facebook.github.io/PathPicker/dist/fpp.$VER
 HASH=$(cat $DEST | shasum -a 256 | cut -d " " -f 1)
 sed -i '' -e "s#sha256 .*#sha256 \"$HASH\"#g" ./fpp.rb
 
-NUMFILES=$(git status -sb | wc -l)
-if (( NUMFILES > 4 )); then
-  echo "Git may include other changes aborting"
-  exit 1
-fi
-
-echo "Pushing distribution update"
-git checkout master && git commit -am "Distribution update" && git push
-git checkout gh-pages && git rebase master && git push
-git checkout master
+echo "Recipe updated with hash from $DEST"
