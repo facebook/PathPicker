792b8e28effd596a6a7ae8a816a5f5e845dc2e13
Peter Cottle <pcottle@fb.com>
2015-05-04 10:36:01 -0700

    update

--- makeDist.sh
+++ makeDist.sh
@@ -4,4 +4,4 @@ DEST="./dist/fpp.$VERSION.tar.gz"
 tar -cf $DEST src/*.py fpp
 git add $DEST
 
-sed -i -e "s#url .*#url \"https://facebook.github.io/PathPicker/dist/fpp.$VERSION.tar.gz\"#g" ./fpp.rb
+sed -i '' -e "s#url .*#url \"https://facebook.github.io/PathPicker/dist/fpp.$VERSION.tar.gz\"#g" ./fpp.rb
