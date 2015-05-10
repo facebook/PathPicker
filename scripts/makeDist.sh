85b9b0b336616f9dfe9bccfd5b04518f4d64dd6a
Peter Cottle <pcottle@fb.com>
2015-05-10 12:18:02 -0700

    fix dist script

--- scripts/makeDist.sh
+++ scripts/makeDist.sh
@@ -8,6 +8,6 @@ tar -cf $DEST src/*.py fpp
 
 sed -i '' -e "s#url .*#url \"https://github.com/facebook/PathPicker/releases/download/$VERSION/fpp.$VERSION.tar.gz\"#g" ./fpp.rb
 HASH=$(cat $DEST | shasum -a 256 | cut -d " " -f 1)
-sed -i '' -e "s#sha256 .*#sha256 \"$HASH\"#g" ./fpp.rb
+sed -i '' -e "s#^  sha256 .*#  sha256 \"$HASH\"#g" ./fpp.rb
 
 echo "Recipe updated with hash from $DEST"
