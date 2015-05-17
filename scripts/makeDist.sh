3a6dbe1e5ba8c1ded1b061cbea3ec8bbfbe41136
Pallav Agarwal <pallavag@iitk.ac.in>
2015-05-17 22:59:31 +0530

    debian folder populated, script makeDist changed

--- scripts/makeDist.sh
+++ scripts/makeDist.sh
@@ -9,3 +9,12 @@ HASH=$(cat $DEST | shasum -a 256 | cut -d " " -f 1)
 sed -i '' -e "s#^  sha256 .*#  sha256 \"$HASH\"#g" ./fpp.rb
 
 echo "Recipe updated with hash from $DEST"
+
+
+# Packaging for debian systems.
+# Creates a .deb file in parent directory of the source tree.
+
+DATETIME=$(date '+%a, %d %b %Y %H:%M:%S %z')
+sed -i.bak s#__version__#"$VERSION"# ./debian/changelog
+sed -i.bak s#__date_timestamp__#"$DATETIME"# ./debian/changelog
+debuild -us -uc -i -I
