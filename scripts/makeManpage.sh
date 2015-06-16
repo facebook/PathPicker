af403cef6b1609fcba8aa9c160a7ed4d3542cefa
Seth Woodworth <seth@sethish.com>
2015-06-15 23:22:49 -0400

    makes man generation script check for a2x before running

--- scripts/makeManpage.sh
+++ scripts/makeManpage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+command -v a2x >/dev/null 2>&1 || { echo >&2 "I require a2x provided by asciidoc, but it's not installed.  Aborting."; exit 1; }
 python src/usageStrings.py > manpage.adoc;
 a2x --doctype manpage --format manpage manpage.adoc --destination-dir ./debian/usr/share/man/man1/;
-
