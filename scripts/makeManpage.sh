840757fa9f1e9a455a5c590ef28de0285d857f0e
Seth Woodworth <seth@sethish.com>
2015-06-15 22:15:09 -0400

    moves manpage to debian package location for manpages

--- scripts/makeManpage.sh
+++ scripts/makeManpage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python src/usageStrings.py > manpage.adoc;
-a2x --doctype manpage --format manpage manpage.adoc;
+a2x --doctype manpage --format manpage manpage.adoc --destination-dir ./debian/usr/share/man/man1/;
 
