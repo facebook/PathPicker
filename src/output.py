02765bed659368763fa03388ca60560cb2ca1460
Peter Cottle <pcottle@fb.com>
2016-09-04 13:40:36 -0400

    Resolves #242 -- fix function call

--- src/output.py
+++ src/output.py
@@ -203,7 +203,7 @@ def appendExit():
     # is no other simple and reliable way to detect the current shell.
     shell = os.environ['SHELL']
     # ``csh``, fish`` and, ``rc`` uses ``$status`` instead of ``$?``.
-    if shell.endswith('csh') or shell.endswith('fish') or shell.endswith['rc']:
+    if shell.endswith('csh') or shell.endswith('fish') or shell.endswith('rc'):
         exit_status = '$status'
     # Otherwise we assume a Bournal-like shell, e.g. bash and zsh.
     else:
