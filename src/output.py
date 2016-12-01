d312b8e40a797faf53bf2d5eefbab234268f3e3c
brwong <github@brwong.net>
2016-12-01 17:39:44 -0500

    refactored output.py to correct vim behavior
    - changed vim output from columns to tabs
    - changed vim command so that jump-to-line commands work

--- src/output.py
+++ src/output.py
@@ -47,12 +47,9 @@ def execComposedCommand(command, lineObjs):
 
 
 def editFiles(lineObjs):
-    partialCommands = []
     logger.addEvent('editing_num_files', len(lineObjs))
-    for lineObj in lineObjs:
-        (path, num) = (lineObj.getPath(), lineObj.getLineNum())
-        partialCommands.append(getEditFileCommand(path, num))
-    command = joinEditCommands(partialCommands)
+    filesAndLineNumbers = [(lineObj.getPath(), lineObj.getLineNum()) for lineObj in lineObjs]
+    command = joinFilesIntoCommand(filesAndLineNumbers)
     appendIfInvalid(lineObjs)
     appendToFile(command)
     appendExit()
@@ -94,18 +91,6 @@ def getEditorAndPath():
     return 'vim', 'vim'
 
 
-def getEditFileCommand(filePath, lineNum):
-    editor, _editor_path = getEditorAndPath()
-    if editor in ['vim', 'vim -p'] and lineNum != 0:
-        return '\'%s\' +%d' % (filePath, lineNum)
-    elif editor in ['vi', 'nvim', 'nano', 'joe', 'emacs',
-                    'emacsclient'] and lineNum != 0:
-        return '+%d \'%s\'' % (lineNum, filePath)
-    elif editor in ['subl', 'sublime', 'atom'] and lineNum != 0:
-        return '\'%s:%d\'' % (filePath, lineNum)
-    else:
-        return "'%s'" % filePath
-
 
 def expandPath(filePath):
     # expand ~/ paths
@@ -113,14 +98,25 @@ def expandPath(filePath):
     # and in case of grep, expand ./ as well
     return os.path.abspath(filePath)
 
-
-def joinEditCommands(partialCommands):
+def joinFilesIntoCommand(filesAndLineNumbers):
     editor, editor_path = getEditorAndPath()
-    if editor in ['vim', 'mvim'] and \
-            not os.environ.get('FPP_DISABLE_SPLIT'):
-        return editor_path + ' -O ' + ' '.join(partialCommands)
-    # Assume that all other editors behave like emacs
-    return editor_path + ' ' + ' '.join(partialCommands)
+    cmd = editor_path + ' '
+    if editor in ['vim', 'vim -p']:
+        firstFilePath, firstLineNum = filesAndLineNumbers[0]
+        cmd += ' +%d %s' % (firstLineNum, firstFilePath)
+        for (filePath, lineNum) in filesAndLineNumbers[1:]:
+            cmd += ' +"tabnew +%d %s"' % (lineNum, filePath)
+    else:
+        for (filePath, lineNum) in filesAndLineNumbers:
+            logger.addEvent('cLINE', filePath);
+            if editor in ['vi', 'nvim', 'nano', 'joe', 'emacs',
+                        'emacsclient'] and lineNum != 0:
+                cmd += ' +%d \'%s\'' % (lineNum, filePath)
+            elif editor in ['subl', 'sublime', 'atom'] and lineNum != 0:
+                cmd += ' \'%s:%d\'' % (filePath, lineNum)
+            else:
+                cmd += " '%s'" % filePath
+    return cmd
 
 
 def composeCdCommand(command, lineObjs):
