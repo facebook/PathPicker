67c161ddcd9e74ddd1ff720a78c2b3483fac100c
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2021-02-11 01:08:16 +0000

    Remove __future__ library

--- src/choose.py
+++ src/choose.py
@@ -2,8 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from __future__ import print_function
-
 import curses
 import pickle
 import sys
