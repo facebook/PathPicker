80af46639931e904b5a9de90438c2096dd8bfd77
Thiago Perrotta <perrotta.thiago@poli.ufrj.br>
2021-07-01 15:45:34 -0400

    Fix man page (#403)
    - Updates man page with newly added flags since 2015. Man page had been
      last updated only in 2015.
    - Fix formatting of flags in man page. Instead of joining lines, now
      each flag is correctly displayed in its own line, making it easier to
      read.
    - Fix man page generation script, which was broken and still referencing
      the old usageStrings.py (correct filename is usage_strings.py).

--- scripts/makeManpage.sh
+++ scripts/makeManpage.sh
@@ -4,6 +4,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 command -v a2x >/dev/null 2>&1 || { echo >&2 "I require a2x provided by asciidoc, but it's not installed.  Aborting."; exit 1; }
-python3 src/usageStrings.py > manpage.adoc;
-a2x --doctype manpage --format manpage manpage.adoc --destination-dir ./debian/usr/share/man/man1/;
-gzip -9 ./debian/usr/share/man/man1/fpp.1;
+(cd src && python3 -m pathpicker.usage_strings > manpage.adoc)
+a2x --format manpage "src/manpage.adoc" --destination-dir debian/usr/share/man/man1/
